c40f6301495390d4e1813f7738e9aedc3c8683ea
moroz <morozovdaniel713@gmail.com>
2025-10-19 14:22:06 +0300

    feat(core): implement config for centralized added variable and import settings

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_DB_NAME: str
+    POSTGRES_USER_NAME: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+@lru_cache
+def get_settings() -> Settings:
+    """Возвращает экземпрял класса настроек"""
+    return Settings()
+
+
+def reload_settings() -> Settings:
+    """Очищает кэш и возвращает обновлённые настройки."""
+    get_settings.cache_clear()
+    return get_settings()
+
+
+settings = get_settings()
