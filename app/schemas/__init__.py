be8e981572eb1adb58913b3dc05dcb9cc0eab5c5
moroz <morozovdaniel713@gmail.com>
2025-10-20 21:49:26 +0300

    refactor(schemas): removed unnecessary fields, move validator for phones

---
+++ app/schemas/__init__.py
@@ -0,0 +1,15 @@
+from app.schemas.activity import Acivity, ActivityCreate
+from app.schemas.building import Building, BuildingCreate
+from app.schemas.organization import Organization, OrganizationCreate
+from app.schemas.phone import Phone, PhoneCreate
+
+__all__ = [
+    "Acivity",
+    "ActivityCreate",
+    "Building",
+    "BuildingCreate",
+    "Organization",
+    "OrganizationCreate",
+    "Phone",
+    "PhoneCreate",
+]
--- app/schemas/organization.py
+++ app/schemas/organization.py
@@ -1,33 +1,19 @@
-import re
 from typing import Annotated
 
-from pydantic import BaseModel, ConfigDict, Field, field_validator
+from pydantic import BaseModel, ConfigDict, Field
 
-from app.schemas.activity import Acivity
-from app.schemas.building import Building
+from app.schemas import Building
 
 
 class OrganizationCreate(BaseModel):
     name: Annotated[str, Field(..., min_length=5, max_length=155)]
-    phones: Annotated[list[str], Field(...)]
-    building_id: Annotated[int, Field(..., ge=1, le=15)]
-    activity_ids: Annotated[list[int], Field(..., min_length=1)]
-
-    @field_validator("phone_numbers")
-    @classmethod
-    def validate_phones(cls, value):
-        pattern = r"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"
-        for phone in value:
-            if not re.match(pattern, phone):
-                raise ValueError(f"Incorrect format of phone number {phone}")
+    building_id: Annotated[int, Field(..., ge=1)]
 
 
 class Organization(BaseModel):
     id: Annotated[int, Field(...)]
     name: str
-    phones: list[str]
     building: Building
-    activities: list[Acivity]
     is_active: Annotated[bool, Field(default=True)]
 
     model_config = ConfigDict(from_attributes=True)
