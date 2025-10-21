efb082a9d4a3c99921b7691085a2fb5c41a35781
moroz <morozovdaniel713@gmail.com>
2025-10-21 14:05:48 +0300

    feat(deps): add dependency for repos

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,10 +1,34 @@
+# ruff:noqa:B008
 from collections.abc import AsyncGenerator
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import async_session_maker
+from app.repositories import (
+    ActivityRepository,
+    BuildingRepository,
+    OrganizationRepository,
+    PhoneRepository,
+)
 
 
 async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as db:
         yield db
+
+
+def get_activity_repository(db: AsyncSession = Depends(get_async_db)):
+    return ActivityRepository(db=db)
+
+
+def get_building_repository(db: AsyncSession = Depends(get_async_db)):
+    return BuildingRepository(db=db)
+
+
+def get_phone_repository(db: AsyncSession = Depends(get_async_db)):
+    return PhoneRepository(db=db)
+
+
+def get_organization_repository(db: AsyncSession = Depends(get_async_db)):
+    return OrganizationRepository(db=db)
---
+++ app/repositories/__init__.py
@@ -0,0 +1,11 @@
+from app.repositories.activities import ActivityRepository
+from app.repositories.buildings import BuildingRepository
+from app.repositories.organizations import OrganizationRepository
+from app.repositories.phones import PhoneRepository
+
+__all__ = [
+    "ActivityRepository",
+    "BuildingRepository",
+    "PhoneRepository",
+    "OrganizationRepository",
+]
